d088bec614b7ff2a128a1b7177a67ef1266572f3
Jinesh <jthomas0024@conestogac.on.ca>
2021-11-18 20:35:49 -0500

    usertest

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 router.get('/',(req,res,next) => {
-  res.render('/error.hbs');
+  res.render('views/error.hbs');
 });
 
 module.exports = router
