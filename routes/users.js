bc96965a143da2ec895db7279cf3d3dee5a22102
Jinesh <jthomas0024@conestogac.on.ca>
2021-11-18 20:43:28 -0500

    usererror404updated

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req,res,next) => {
-  res.render('/error');
+  res.render('error');
 });
 
 module.exports = router
