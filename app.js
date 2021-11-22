e18ce8fb6db48e4802219e4860fceb4813ab171c
Jinesh <jthomas0024@conestogac.on.ca>
2021-11-22 01:49:47 -0500

    220149

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/about');
 var projectcontroller = require('./routes/project');
 var skillsController = require('./routes/skills');
 var contactController = require('./routes/contact');
