add2ee64cdbaca3e73ba21ec124befbceaac4959
Jinesh <jthomas0024@conestogac.on.ca>
2021-11-22 00:27:46 -0500

    220027

--- routes/project.js
+++ routes/project.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req,res,next) => {
-    res.render('Projects/index', {projectTitle1:'ABC Orthopedics',
+    res.render('Projects/index', { projectTitle1:'ABC Orthopedics',
     projectOneDesc:'ABC Orthopedic Hospital is the centre of excellence for delivering world class orthopedic care affordable to the people of Kerala. The website has made in wordpress with a timeframe of 2 weeks.',
     projectTitle2:'gadielprefab',
     projectTwoDesc:'GADIEL PRE-FAB DESIGNS & CONTRACTING LLP is a construction company in Kochi,India providing Eco-Friendly homes to all for residential as well as for commercial purpose with the power of PRE-FAB construction technology. The website is built in wordpress.',
