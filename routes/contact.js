9cad0ee3445e7f0dcd68f074b6c4b5aa5e339eb0
Jinesh <jthomas0024@conestogac.on.ca>
2021-11-22 02:35:08 -0500

    220225

--- routes/contact.js
+++ routes/contact.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req,res,next) => {
-  res.render('contact', { introContact: 'You can reach me at given social media accounts, email and phone number. Happy to be connect with you!',
+  res.render('contact', { introContact: 'You can reach me at given social media accounts, email and phone number.',
   phone:'Phone Number: +1-4379295262',
   email:'e-mail: jthomas0024@conestogac.on.ca',
   address:'Address: 346 Dale Cresent, Waterloo, ON N2J3Y3'
