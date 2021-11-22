505867d16dfe6efba5f4f3652f214b42d613a8eb
Jinesh <jthomas0024@conestogac.on.ca>
2021-11-22 00:01:51 -0500

    220001

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,10 @@ var router = express.Router();
 /* GET home page. */
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'Basic Info', 
-  pageText:'Hello, Myself Jinesh Thomas\n I am a web developer passionate in deveveloping web techologies',
-  var:'This about my page'
+  greeting:'Hi,',
+  name:'I am Jinesh',
+  position: 'Web Developer',
+  intro: 'An enthusiastic web developer possesses skills in all the latest web development technologies and looks forward to contributing to team success through dedication, hard work, and efficient management skills. Specialized in Mobile Solutions Development from Conestoga college, having detailed knowledge regarding Android and ios platforms. I also have experience in all the stages of software development and five years of experience in the field of web development.'
  });
 });
 
